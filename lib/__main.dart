06134e64e3fe1244ff3907f4ec2a58383430b0e8
ひろろ <hrsi.ad@gmail.com>
2022-07-03 10:40:23 +0900

    TweetUI 分離

---
+++ lib/__main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  print("test");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/TweetTile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,43 +56,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TweetTile extends StatelessWidget {
-  const TweetTile({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start, // 上揃えにする
-        children: [
-          const CircleAvatar(
-            backgroundImage: NetworkImage('https://img.gamewith.jp/article_tools/pad/gacha/8932.png'),
-          ),
-          const SizedBox(width: 8),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // 左
-            children: [
-              Row(children: const [
-                Text('しゃーくど'),
-                SizedBox(width: 8), // 隙間
-                Text('2022/05/05'),
-              ],
-              ),
-              const SizedBox(height: 14),
-              const Text('最高でした。'),
-              IconButton(
-               onPressed: () {},
-               icon: const Icon(Icons.favorite_border),
-             ),
-
-            ],
-
-          ),
-        ],
-      ),
-    );
-  }
-}
